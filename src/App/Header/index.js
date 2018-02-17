fba4cdc33d668c08d5e5dc94c425bec719aef0cc
Paaaaaaul <paulmeyerber@gmail.com>
2018-02-17 00:52:22 -0600

    basics of header and footer, needs to be made responsive

---
+++ src/App/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { } from 'semantic-ui-react'
+import './index.css'
+const Header = () => (
+  <div className='header'>
+    <h2>Header</h2>
+  </div>
+)
+export default Header
