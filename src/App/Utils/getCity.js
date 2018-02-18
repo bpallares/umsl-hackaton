5d9ed7382e0c77d63b0f62ca95cfc3e05fd42eac
Boris Pallares <borispallares@hotmail.com>
2018-02-17 21:43:50 -0600

    getCity bedrooms util added

--- src/App/Utils/getCity.js
+++ src/App/Utils/getCity.js
@@ -1,7 +1,14 @@
 import CityCoords from './cities.json'
+import BedRooms from './bedrooms.json'
 
 export const getCityObject = (cityNeeded) => {
   return CityCoords.find((element) => (
     element.city === cityNeeded
+  ))
+}
+
+export const getBedRooms = (cityNeeded) => {
+  return BedRooms.find((element) => (
+    element.RegionName === cityNeeded
   ))
 }
